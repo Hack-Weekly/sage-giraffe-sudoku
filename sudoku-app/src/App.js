b79966a3b06d2a93c75aaaf73eef91af10eb3b28
Mufaddal Naguthanawala <m.nguthana@hotmail.com>
2023-08-14 11:11:11 -0700

    add timer for different levels

--- sudoku-app/src/App.js
+++ sudoku-app/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 import SudokuGrid from './SudokuGrid';
 
 function App() {
@@ -20,13 +21,30 @@ function Header() {
 }
 
 function Buttons() {
-  return(
-    <div class="Buttons">
-      <button class="button green">Easy</button>
-      <button class="button blue">Medium</button>
-      <button class="button red">Hard</button>
+  const [remainingTime, setRemainingTime] = useState(0);
+
+  useEffect(() => {
+    if (remainingTime > 0) {
+      const timer = setInterval(() => {
+        setRemainingTime(prevTime => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [remainingTime]);
+
+  const startTimer = (minutes) => {
+    setRemainingTime(minutes * 60); 
+  };
+
+  return (
+    <div className="Buttons">
+      <button className="button green" onClick={() => startTimer(20)}>Easy</button>
+      <button className="button blue" onClick={() => startTimer(15)}>Medium</button>
+      <button className="button red" onClick={() => startTimer(10)}>Hard</button>
+      <div className="timer">Time remaining: {Math.floor(remainingTime / 60)}:{(remainingTime % 60).toString().padStart(2, '0')}</div>
     </div>
-  )
+  );
 }
 
 export default App;
