c82d10ff8d57840c9b2b5be7ad92945d7105aeab
lly02 <lly.02@outlook.com>
2023-08-14 15:29:26 +0800

    feat: sudoku grid borders

--- sudoku-app/src/SudokuGrid.js
+++ sudoku-app/src/SudokuGrid.js
@@ -9,7 +9,10 @@ const SudokuGrid = ({ grid, handleInputChange }) => {
             <input
               key={colIndex}
               type="text"
-              className="sudoku-cell"
+              className={
+                `sudoku-cell 
+                ${rowIndex == 3 || rowIndex == 6 ? "t-border" : ""} 
+                ${colIndex + 1 == 3 || colIndex + 1 == 6 ? "r-border" : ""}`}
               value={cellValue}
               maxLength="1"
               onChange={(e) =>
