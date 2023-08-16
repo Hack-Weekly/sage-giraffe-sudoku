4c8a2b237227a3088cc70868e4e3516e6cdfc392
random56428 <55150488+random56428@users.noreply.github.com>
2023-08-16 14:16:52 -0700

    Fixed header position to be centered

--- sudoku-app/src/Header.js
+++ sudoku-app/src/Header.js
@@ -1,6 +1,7 @@
 function Header() {
   return (
     <div className="Header">
+      <div style={{ padding: 20 }}></div>
       <h1>Sagedoku</h1>
       <button className="toggle-theme"></button>
     </div>
